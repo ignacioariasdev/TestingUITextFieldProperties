ebb4cc67accd20a42521362e509443814afa6bc1
Ignacio Arias <1992ignacio@gmail.com>
2021-01-26 03:32:51 -0800

    Initial Commit

---
+++ TestingUITextFieldProperties/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingUITextFieldProperties
+//
+//  Created by Ignacio Arias on 2021-01-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
